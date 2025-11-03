dd1b6de5aa06df0b962f6c33dc858f81d86c2d0f
Benzine <fryfonbly@gmail.com>
2025-11-03 08:07:39 +0100

    Update time string format

--- Code/SplitsMgr/Utils.h
+++ Code/SplitsMgr/Utils.h
@@ -39,7 +39,7 @@ namespace SplitsMgr
 		std::string get_xml_child_element_text( tinyxml2::XMLElement* _container, std::string_view _child_name );
 		void create_xml_child_element_with_text( tinyxml2::XMLDocument& _document, tinyxml2::XMLElement* _container, std::string_view _child_name, std::string_view _text );
 
-		SplitTime get_time_from_string( std::string_view _time, std::string_view _format = "%H:%M:%S" );
+		SplitTime get_time_from_string( std::string_view _time, std::string_view _format = "%4H:%M:%S" );
 		std::string time_to_str( const SplitTime& _time, bool _floor_seconds = true, bool _separate_days = false );
 		bool is_time_valid( const SplitTime& _time );
 	}
